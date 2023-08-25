d40f837daf96e7481f71c75e43d8fdd30b237016
Vu <tranvu221097@gmail.com>
2023-08-26 06:00:48 +0700

    update env

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { adminItem } from './constants/admin.js';
 
 const User = db.user;
 
-const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@server.ukormm7.mongodb.net/commerce-web`;
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}${process.env.MONGODB_SERVER}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
 
 const app = express()
 
